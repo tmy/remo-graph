b2fd696bae8358250612efe58c6f9798d29886cb
Akinori Tomita <tommy@328w.co.jp>
2023-12-15 17:09:19 +0900

    TF 1.6 にバージョンアップ
    加えて terraform providers lock -platform=darwin_arm64 -platform=darwin_amd64 を実行

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,23 @@
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = ">= 1.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.31.0"
+    }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.4.0"
+    }
+  }
 }
 
 provider "aws" {
-  version = "~> 2.48"
 }
 
 provider "archive" {
-  version = "~> 1.3"
 }
 
 data "aws_caller_identity" "self" {}
